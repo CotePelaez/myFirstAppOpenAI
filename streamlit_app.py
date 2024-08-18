bdb4f9e79216327d65eedef0df5455c5c0e29408
Maria Jose Peláez <maria.pelaez@mariapelaez-MacBook-Pro-CGXF.local>
2024-08-19 00:26:59 +0200

    Arreglos esteticos

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,22 +2,45 @@ import streamlit as st
 from openai import OpenAI
 import os
 
+# Inyectar CSS en la aplicación
+st.markdown("""
+    <style>
+    .stButton button {
+        background-color:   #c5888a  ;
+        color: white;
+        font-size: 34px;
+        height: 80px;
+        width: 300px;
+        border-radius: 10px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
 # Show title and description.
 st.title("Planea tu próximo viaje")
-st.image("caratula.jpeg", use_column_width=True)
+st.image("caratula3.jpeg", use_column_width=True)
 
 st.write(
-    "Vamos a intentar crear tu itinerario personalizado"
-    "del viaje que deseas hacer"
+    "Vamos a crear tu itinerario personalizado"
+    " del viaje que deseas hacer"
 )
 
 # Parámetros del usuario
-user_pais_input = st.text_input("Lugar", "Dime qué país, ciudad o área del mundo quieres visitar")
-user_dias_input = st.text_input("Días", "¿Cuántos días estarás?")
-user_company_input = st.text_input("Gente", "Cuéntame si viajarás solo, con pareja, con familia, en general con cuántas personas")
-user_restricciones_input = st.text_input("Restricciones", "Cuéntame algo que no quieres para este viaje")
-user_intereses_input = st.text_input("Intereses", "Cuéntame qué tipo de intereses tienes: comida, vida nocturna, montaña, etc.")
-user_dinero_input = st.text_input("Dinero", "Cuéntame cuánto dinero estás pensando en gastar")
+user_pais_input = st.text_input(label="Lugar", value="", key="hidden_input1", 
+                                placeholder="Dime qué país, ciudad o área del mundo quieres visitar",
+                                autocomplete = "on")
+
+#user_pais_input = st.text_input("Lugar", "Dime qué país, ciudad o área del mundo quieres visitar",autocomplete = "on")
+user_dias_input = st.text_input(label="Días", value="", key="hidden_input2",
+                                placeholder="¿Cuántos días estarás?")
+user_company_input = st.text_input(label="Gente",value="", key="hidden_input3",
+                                placeholder="Cuéntame si viajarás solo, con pareja, con familia, en general con cuántas personas")
+user_intereses_input = st.text_input(label="Intereses", value="", key="hidden_input5",
+                                     placeholder="Cuéntame qué tipo de intereses tienes: comida, vida nocturna, montaña, etc.")
+user_dinero_input = st.text_input(label="Dinero", value="", key="hidden_input6",
+                                  placeholder="Cuéntame cuánto dinero estás pensando en gastar")
+user_restricciones_input = st.text_input(label="Restricciones", value="", key="hidden_input4",
+                                         placeholder="Cuéntame algo que no quieres para este viaje")
 
 # Generar el prompt
 prompt = f"""
